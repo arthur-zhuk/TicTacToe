2640537d4d71c4023d8df1a424031b0395875c80
ug02fast <arthurzhuk@gmail.com>
2016-11-16 08:29:35 -0800

    ai started

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,18 @@ var TicTacClient = React.createClass({
         "", "", "",
         "", "", ""
       ],
+      rows: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        [0, 4, 8],
+        [2, 4, 6]
+      ],
       winScore: { player: 0, ai: 0 },
       winTiles: [
         2, 7, 6,
@@ -26,14 +38,47 @@ var TicTacClient = React.createClass({
     console.log(this.state.winScore);
 
     tiles[id] = this.state.turn;
-    //debugger;
+
     winScore.player += winTiles[id];
 
-    this.setState({ tiles: tiles, turn: this.state.turn === "o" ? "x" : "o", winScore: winScore });
-    //checkWinner();
+    this.setState({ tiles: tiles, turn: this.state.turn = "o" ? "x" : "o", winScore: winScore });
+    this.checkWinner();
+    // After each click move to the AI's turn.
+    this.aiTurn();
   },
   checkWinner: function() {
-
+    var tiles = this.state.tiles;
+    // Map through. Check if row contains all of 'o' or 'x' then this.state.turn wins message appears.
+    this.state.rows.map(function(row) {
+      return row.every(function(element){
+        if (tiles[element] === "o") {
+          return console.log(tiles[element] + " every element in this row is " + tiles[element]);
+        }
+      })
+    });
+  },
+  // Could also remember the ID in an array and write the code to ignore all Id's who've been selected previously.
+  isValid: function(chosenNumber) {
+    var tiles = this.state.tiles;
+    if (tiles[chosenNumber] !== "") {
+      console.log(chosenNumber);
+      this.isValid(Math.floor(Math.random() * (8 - 0 + 1)) + 0);
+    } else {
+      tiles[chosenNumber] = this.state.turn;
+    }
+  },
+  aiTurn: function() {
+    //this.setState({ turn: this.state.turn ===})
+	  var randomNumber;
+    var tiles = this.state.tiles;
+	tiles[0] = "oo";
+	if (tiles[0][1] === "o")
+		  alert("it's 2d");
+	  else if (tiles[2].includes("o"))
+	  randomNumber = 5;
+    //var randomNumber = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+    this.isValid(randomNumber);
+    this.setState({ turn: this.state.turn = "x" ? "o" : "x" });
   },
   render: function() {
     var handler = this.handleClick;
